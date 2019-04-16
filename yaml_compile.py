784c952e622ead55f72b0bd566904fc2277657ef
alexanderjones19 <alexanderjones4215@gmail.com>
2019-04-16 16:12:13 -0400

    code cleanup

--- yaml_compile.py
+++ yaml_compile.py
@@ -6,49 +6,36 @@ merge_template = sys.argv[1]
 
 class Loader(yaml.SafeLoader):
 
-    def __init__(self, stream):
+  def __init__(self, stream):
 
-        self._root = os.path.split(stream.name)[0]
+    self._root = os.path.split(stream.name)[0]
 
-        super(Loader, self).__init__(stream)
+    super(Loader, self).__init__(stream)
 
-    def include(self, node):
+  def include(self, node):
 
-        print('include', node)
-        print('scalar', self.construct_scalar(node))
-        filename = os.path.join(self._root, self.construct_scalar(node))
+    filename = os.path.join(self._root, self.construct_scalar(node))
 
-        with open(filename, 'r') as f:
-            data = yaml.load(f, Loader)
-            print(data)
-            return data
+    with open(filename, 'r') as f:
+      data = yaml.load(f, Loader)
+      return data
 
 Loader.add_constructor('!include', Loader.include)
 
 def expand_function(self, node):
-  print(node)
   output = {'Fn::' + node.tag[1:]: node.value}
-  # output = '\n' + 'Fn::' + node.tag[1:] + ': ' + node.value
-  print(output)
+  return output
+
+def expand_helper(self, node):
+  output = {node.tag[1:]: node.value}
   return output
 
 Loader.add_constructor('!Sub', expand_function)
 Loader.add_constructor('!GetAtt', expand_function)
-Loader.add_constructor('!Ref', expand_function)
+Loader.add_constructor('!Ref', expand_helper)
 Loader.add_constructor('!Join', expand_function)
-
-# for filename in os.listdir('templates/'):
-#   if filename.endswith(".yaml"): 
-#     print(filename)
-#         # print(os.path.join(directory, filename))
-#     continue
-#   else:
-#     continue
 
 with open('template.yaml', 'r') as f:
   data = yaml.load(f, Loader)
   stream = file(merge_template, 'w')
   yaml.safe_dump(data, stream) 
-
-    # yaml.dump(data, outfile, default_flow_style=False)
-  print(data)
