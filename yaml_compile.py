0c520d6f5fd602236d673a7a400f0e7686c504d8
Joseph Jones <josephzacharyjones@gmail.com>
2019-05-17 16:19:25 -0400

    impove deploy script, add output

--- yaml_compile.py
+++ yaml_compile.py
@@ -14,22 +14,16 @@ class Loader(yaml.SafeLoader):
 
     def include(self, node):
 
-        print('include', node)
-        print('scalar', self.construct_scalar(node))
         filename = os.path.join(self._root, self.construct_scalar(node))
 
         with open(filename, 'r') as f:
             data = yaml.load(f, Loader)
-            print(data)
             return data
 
 Loader.add_constructor('!include', Loader.include)
 
 def expand_function(self, node):
-  print(node)
   output = {'Fn::' + node.tag[1:]: node.value}
-  # output = '\n' + 'Fn::' + node.tag[1:] + ': ' + node.value
-  print(output)
   return output
 
 Loader.add_constructor('!Sub', expand_function)
@@ -37,18 +31,7 @@ Loader.add_constructor('!GetAtt', expand_function)
 Loader.add_constructor('!Ref', expand_function)
 Loader.add_constructor('!Join', expand_function)
 
-# for filename in os.listdir('templates/'):
-#   if filename.endswith(".yaml"): 
-#     print(filename)
-#         # print(os.path.join(directory, filename))
-#     continue
-#   else:
-#     continue
-
 with open('template.yaml', 'r') as f:
   data = yaml.load(f, Loader)
   stream = file(merge_template, 'w')
   yaml.safe_dump(data, stream) 
-
-    # yaml.dump(data, outfile, default_flow_style=False)
-  print(data)
